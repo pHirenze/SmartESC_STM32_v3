ddb9b23180b353e3b49f4e3bf3241a72d98e0edc
pHirenze <phirenze@pinja.org>
2022-07-29 17:16:18 +0300

    Circ + gears

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -44,27 +44,27 @@
 
 // parameters for speed calculation
 #define WHEEL_CIRCUMFERENCE 710 //690 for original M365 motor
-#define GEAR_RATIO 11 //15 for original M365 motor
+#define GEAR_RATIO 15 //15 for original M365 motor
 
 // speed limits for invividual modes in kph
 #define SPEEDLIMIT_ECO 16
-#define SPEEDLIMIT_NORMAL 20
+#define SPEEDLIMIT_NORMAL 30
 #define SPEEDLIMIT_SPORT 50
 
 // motor current limits for invividual modes in mA, see default settings at https://max.cfw.sh/#
 #define PH_CURRENT_MAX_ECO 16000
-#define PH_CURRENT_MAX_NORMAL 20000
-#define PH_CURRENT_MAX_SPORT 25000
+#define PH_CURRENT_MAX_NORMAL 28000
+#define PH_CURRENT_MAX_SPORT 40000
 
 // motor current limit for regen in mA
-#define REGEN_CURRENT 4000
+#define REGEN_CURRENT 15000
 
 // maximum current for flux weakening in mA
 #define FW_CURRENT_MAX 18000 //max id
 
 // maximum battery currents in mA
 #define BATTERYCURRENT_MAX 14500
-#define REGEN_CURRENT_MAX 4000
+#define REGEN_CURRENT_MAX 10000
 
 // battery voltage limits in mV
 #define BATTERYVOLTAGE_MIN 33000
