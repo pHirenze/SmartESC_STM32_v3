744ce6a358280d643a3f1cd46cdd654e050c1a1f
pHirenze <phirenze@pinja.org>
2022-07-31 19:57:37 +0300

    Sunday drive

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -47,13 +47,13 @@
 #define GEAR_RATIO 20 //15 for original M365 motor
 
 // speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 16
+#define SPEEDLIMIT_ECO 26
 #define SPEEDLIMIT_NORMAL 36
 #define SPEEDLIMIT_SPORT 50
 
 // motor current limits for invividual modes in mA, see default settings at https://max.cfw.sh/#
-#define PH_CURRENT_MAX_ECO 16000
-#define PH_CURRENT_MAX_NORMAL 28000
+#define PH_CURRENT_MAX_ECO 20000
+#define PH_CURRENT_MAX_NORMAL 32000
 #define PH_CURRENT_MAX_SPORT 40000
 
 // motor current limit for regen in mA
@@ -63,7 +63,7 @@
 #define FW_CURRENT_MAX 8000 //max id
 
 // maximum battery currents in mA
-#define BATTERYCURRENT_MAX 14500
+#define BATTERYCURRENT_MAX 15000
 #define REGEN_CURRENT_MAX 10000
 
 // battery voltage limits in mV
@@ -76,7 +76,7 @@
 
 // settings for speed PLL (angle estimation)
 #define SPEED_PLL 1 //1 for using PLL, 0 for angle extrapolation
-#define P_FACTOR_PLL 7 //7 for original M365 motor
-#define I_FACTOR_PLL 7 //7 for original M365 motor
+#define P_FACTOR_PLL 8 //7 for original M365 motor
+#define I_FACTOR_PLL 8 //7 for original M365 motor
 
 #endif /* CONFIG_H_ */
