5075e82c5aed897273cb19477dda582120774fb9
pHirenze <phirenze@pinja.org>
2022-07-31 22:18:30 +0300

    Gonna burn

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -40,21 +40,21 @@
 #define THROTTLEOFFSET 45
 #define THROTTLEMAX 175
 #define BRAKEOFFSET 50
-#define BRAKEMAX 190
+#define BRAKEMAX 175
 
 // parameters for speed calculation
 #define WHEEL_CIRCUMFERENCE 715 //690 for original M365 motor
 #define GEAR_RATIO 20 //15 for original M365 motor
 
 // speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 28
-#define SPEEDLIMIT_NORMAL 32
+#define SPEEDLIMIT_ECO 27
+#define SPEEDLIMIT_NORMAL 30
 #define SPEEDLIMIT_SPORT 50
 
 // motor current limits for invividual modes in mA, see default settings at https://max.cfw.sh/#
-#define PH_CURRENT_MAX_ECO 20000
-#define PH_CURRENT_MAX_NORMAL 40000
-#define PH_CURRENT_MAX_SPORT 40000
+#define PH_CURRENT_MAX_ECO 25000
+#define PH_CURRENT_MAX_NORMAL 55000
+#define PH_CURRENT_MAX_SPORT 55000
 
 // motor current limit for regen in mA
 #define REGEN_CURRENT 15000
@@ -63,7 +63,7 @@
 #define FW_CURRENT_MAX 10000 //max id
 
 // maximum battery currents in mA
-#define BATTERYCURRENT_MAX 20000
+#define BATTERYCURRENT_MAX 25000
 #define REGEN_CURRENT_MAX 10000
 
 // battery voltage limits in mV
