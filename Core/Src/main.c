6f9ff1a5c48501a8107d7b768d0fd967d8389264
pHirenze <phirenze@pinja.org>
2023-01-20 16:14:17 +0200

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -411,8 +411,8 @@ int main(void) {
 	/* Initialize all configured peripherals */
  	MX_GPIO_Init();
 	MX_DMA_Init();
-	MX_USART1_UART_Init();
-	MX_USART1_UART_Init();
+//	MX_USART1_UART_Init();
+	MX_USART2_UART_Init();
 	MX_USART3_UART_Init();
 
 	//initialize MS struct.
