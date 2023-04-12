2c1166737910ec444e5789fc8770402af8dece10
pHirenze <phirenze@pinja.org>
2023-04-12 18:24:54 +0300

    Update config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -43,8 +43,8 @@
 #define BRAKEMAX 140
 
 // parameters for speed calculation
-#define WHEEL_CIRCUMFERENCE 705 //770 for gen2, 705 for
-#define GEAR_RATIO 20 //15 for original M365 motor
+#define WHEEL_CIRCUMFERENCE 755 //770 for gen2, 705 for
+#define GEAR_RATIO 15 //15 for original M365 motor
 
 // speed limits for invividual modes in kph
 #define SPEEDLIMIT_ECO 70
@@ -60,7 +60,7 @@
 #define REGEN_CURRENT 14000
 
 // maximum current for flux weakening in mA
-#define FW_CURRENT_MAX_ECO 8000 //max id
+#define FW_CURRENT_MAX_ECO 0 //max id
 #define FW_CURRENT_MAX_SPORT 0 //max id
 #define FW_MINSPEED 24
 
