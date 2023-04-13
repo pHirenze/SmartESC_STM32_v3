d62c6313c6191be863c00bb14889ef6f229c4be7
pHirenze <phirenze@pinja.org>
2023-04-13 19:00:44 +0300

    PPL 9 21kmh

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -78,7 +78,7 @@
 
 // settings for speed PLL (angle estimation)
 #define SPEED_PLL 1 //1 for using PLL, 0 for angle extrapolation
-#define P_FACTOR_PLL 11 //7 for original M365 motor
-#define I_FACTOR_PLL 11 //7 for original M365 motor
+#define P_FACTOR_PLL 9 //7 for original M365 motor
+#define I_FACTOR_PLL 9 //7 for original M365 motor
 
 #endif /* CONFIG_H_ */
